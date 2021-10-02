a16460f687b8a806acf75153bade6fab22fb8f27
allan galdino <galdinoallancodes@gmail.com>
2021-10-02 14:38:23 -0300

    Initial Commit

---
+++ Todoey/TodoeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoeyApp.swift
+//  Todoey
+//
+//  Created by allan galdino on 02/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TodoeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
