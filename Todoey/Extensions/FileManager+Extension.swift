0b5b0d0ddef84ec383b21819b359a432dd9a8b8b
allan galdino <galdinoallancodes@gmail.com>
2021-10-05 21:50:15 -0300

    wip dataStore refactored to save the to dos locally

---
+++ Todoey/Extensions/FileManager+Extension.swift
@@ -0,0 +1,39 @@
+//
+//  FileManager+Extension.swift
+//  Todoey
+//
+//  Created by allan galdino on 04/10/21.
+//
+
+import Foundation
+
+let fileName = "toDos.json"
+
+extension FileManager {
+    static var docDirURL: URL {
+        Self.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    }
+
+    func saveDocument(contents: String, docName: String, completion: (ToDoError?) -> Void) {
+        let url = Self.docDirURL.appendingPathComponent(docName)
+        do {
+            try contents.write(to: url, atomically: true, encoding: .utf8)
+        } catch {
+            completion(.saveError)
+        }
+    }
+
+    func readDocument(docName: String, completion: (Result<Data, ToDoError>) -> Void) {
+        let url = Self.docDirURL.appendingPathComponent(docName)
+        do {
+            let data = try Data(contentsOf: url)
+            completion(.success(data))
+        } catch {
+            completion(.failure(.readError))
+        }
+    }
+
+    func docExists(named docName: String) -> Bool {
+        return fileExists(atPath: Self.docDirURL.appendingPathComponent(docName).path)
+    }
+}
